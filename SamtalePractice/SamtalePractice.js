2e0fe8002bb7e6e11d6e9f997c1c3e30d75af4f2
EzTheo <theodor.berghansen@icloud.com>
2022-05-29 06:05:57 +0200

    Started working on GridGameV2

--- SamtalePractice/SamtalePractice.js
+++ SamtalePractice/SamtalePractice.js
@@ -1,1 +1,38 @@
-console.log("Hallo");
+const stylesheet = new CSSStyleSheet();
+document.adoptedStyleSheets.push(stylesheet);
+class Test {
+    constructor({ matrixRows, matrixColumns, matrix }) {
+        this.items = [];
+        this.matrixRows = matrixRows;
+        this.matrixColumns = matrixColumns;
+        this.matrix = matrix
+            ? new Array(this.matrixRows).fill(0).map(() => new Array(this.matrixColumns).fill(0))
+            : undefined; //Muligens bruke ?. på disse
+    }
+    createGrid() {
+        stylesheet.addRule(
+            "body",
+            `grid-template-rows: repeat(auto-fill, ${100 / this.matrixRows}vh);
+            grid-template-columns: repeat(auto-fill, ${100 / this.matrixColumns}vw)`
+        );
+        /*         for (let y = 0; y < this.matrix.length; y++)
+            for (let x = 0; x < this.matrix[y].length; x++) {
+                console.log(this.matrix[y][x]);
+            } */ //Vet ikke hva jeg skulle med dette
+        for (let x in this.items) {
+            document.querySelector("body").append(this.items[x]);
+        }
+        return this;
+    }
+    createItem(type, className) {
+        const newItem = document.createElement(type);
+        newItem.className = className;
+        this.items.push(newItem);
+        return this;
+    }
+}
+
+const test = new Test({ matrixRows: 2, matrixColumns: 4, matrix: true })
+    .createItem("img", "ghost") //Hent inn en JSON fil med de ulike tingene. Ghost har matrixValue 1 osv
+    .createItem("img", "grid")
+    .createGrid();
