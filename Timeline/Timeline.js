a60f7590f115c7eb6dc82e1fa7b24d18c6daf014
EzTheo <theodor.berghansen@icloud.com>
2022-06-06 20:15:03 +0200

    Finished first prototype

--- Timeline/Timeline.js
+++ Timeline/Timeline.js
@@ -1,15 +1,40 @@
+//Object Classes
 class Box {
     constructor({ type, className, parent } = {}) {
-        const newBox = createHTMLElement({
+        this.box = createHTMLElement({
             type: type ?? "div",
             className: className ?? "box",
             parent: parent ?? undefined,
         });
-        newBox.addEventListener("click", this.clicked.bind(this));
+        this.box.addEventListener("click", this.clicked.bind(this));
         return this;
     }
     clicked() {
-        console.log("click");
+        const popUp = createHTMLElement({ type: "div", id: "popup", parent: "#timeline" });
+        const newContainer = createHTMLElement({
+            type: "div",
+            id: "popup-container",
+            parent: "#popup",
+        });
+        newContainer.innerHTML = this.box.innerHTML;
+        const popUpCross = createHTMLElement({
+            type: "div",
+            id: "popup-cross",
+            parent: "#popup-container",
+        });
+        popUpCross.addEventListener("click", () => document.querySelector("#popup").remove());
+        return this;
+    }
+    addTitle(title) {
+        this.box.innerHTML = title;
+        return this;
+    }
+    addTag(name, value) {
+        this.box.setAttribute(name, value);
+        return this;
+    }
+    removeTag(name) {
+        this.box.removeAttribute(name);
         return this;
     }
 }
@@ -17,31 +42,28 @@ class Observasjon extends Box {
     constructor() {
         super({ parent: "#observations-container" });
     }
-    clicked() {
-        console.log("Observasjon");
-    }
 }
 class Hendelse extends Box {
     constructor() {
         super({ parent: "#events-container" });
     }
-    clicked() {
-        console.log("hendelse");
-    }
 }
-const createHTMLElement = ({ type, id, className, parent }) => {
+
+//Objects
+const test1 = new Observasjon().addTag("tag1", true).addTitle("Test");
+const test2 = new Hendelse().addTitle("Test 2");
+const test4 = new Hendelse().addTitle("Test");
+const test5 = new Observasjon().addTitle("Test");
+const test6 = new Hendelse().addTitle("Test");
+const test7 = new Hendelse().addTitle("Test");
+const test8 = new Hendelse().addTitle("Test");
+const test9 = new Hendelse().addTitle("Test");
+
+//Functions
+function createHTMLElement({ type, id, className, parent }) {
     const newElement = document.createElement(type);
     newElement.id = id;
     newElement.className = className;
     document.querySelector(parent).appendChild(newElement);
     return newElement;
-};
-
-const test1 = new Observasjon();
-const test2 = new Hendelse();
-const test4 = new Hendelse();
-const test5 = new Observasjon({});
-const test6 = new Hendelse();
-const test7 = new Hendelse();
-const test8 = new Hendelse();
-const test9 = new Hendelse();
+}
